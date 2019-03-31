71813d5c1459aec0c7c1a44d099fb565bc1583c2
Jon Church <me@jonchurch.com>
2019-03-30 22:35:44 -0400

    disable readline

--- services/console.js
+++ services/console.js
@@ -1,24 +1,27 @@
 const readline = require('readline')
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-})
 
 module.exports = (app, controller) => {
-  rl.on('line', input => {
-    const triggerEvent = {
-      service: 'console',
-      triggerType: 'readLine',
-      payload: {
-        input,
-      },
-    }
-    controller.emit('trigger.', {
-      triggerChannel: 'console/readLine',
-      triggerEvent,
-    })
-  })
+	function startReadLineInput() {
+		const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+		})
+		rl.on('line', input => {
+			const triggerEvent = {
+				service: 'console',
+				triggerType: 'readLine',
+				payload: {
+					input,
+					},
+				}
+			controller.emit('trigger.', {
+				triggerChannel: 'console/readLine',
+				triggerEvent,
+			})
+		})
+		}
+	// startReadLineInput()
 
   const log = message => console.log(`:: ${message}`)
   controller.on('console/log', ({ payload: { message } }) => log(message))
